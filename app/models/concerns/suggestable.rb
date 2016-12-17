f0f4ad82bffb6ebc5ab744f2c36abafc5de0fe7e
bhargav <bhargav.r.raut@gmail.com>
2016-12-17 10:31:13 +0530

    added suggestion search logic

--- app/models/concerns/suggestable.rb
+++ app/models/concerns/suggestable.rb
@@ -13,19 +13,40 @@ module Suggestable
 
 	##we want to take the search results, from es, plus an other pos words that are not in the es results, and make suggestions out of them.
 	def build_suggestions
+		response = search
+		response.hits.hits.each do |res|
+			res.hightlight.name.each do |frag|
+				##here we have to parse it out of the em tags.
+				##then add it to the suggestions.
+				stripped = ActionView::Base.full_sanitizer.sanitize(frag)
+				suggestion = Suggestion.new(:phrase => stripped)
+				if s[stripped].nil?
+					s[stripped] = [suggestion]
+				else
+					s[stripped] << suggestion
+				end
+				
+			end
+		end
 		##populate the hash of suggestions.
 		s = {}	
 	end
 
+	##Searches in elasticsearch for all documents in the 'business' index, 
 	##@return : a Hashie::Mash object of es search results.
-	def search_sentence_in_es
+	def search
 		response = Manager.gateway.client.search index: INDEX_NAME,
               body: {
                 query: { 
                 	match: { 
                 		name: self.sentence 
                 	} 
-                }
+                },
+			    highlight: {
+			        fields: {
+			            name: {}
+			        }
+			    }
             }
         return Hashie::Mash.new response
 	end
--- app/models/manager.rb
+++ app/models/manager.rb
@@ -2,4 +2,27 @@ class Manager
   
   include Elasticsearch::Persistence::Model
 
+  def self.test_insert
+  	response = Manager.gateway.client.create index: "business", type: "thing", body: {
+  		name: "patient"
+  	}
+  	puts response.to_s
+  end
+
+  def self.test_search
+  	Hashie::Mash.new (Manager.gateway.client.search index: "business",
+              body: {
+                query: { 
+                	match: { 
+                		name: "patients, doctors, appointments" 
+                	} 
+                },
+			    highlight: {
+			        fields: {
+			            name: {}
+			        }
+			    }
+    }) 
+  end
+
 end
